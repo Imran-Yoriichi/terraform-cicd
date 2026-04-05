a0dc2a3e7cf697d40bd4d0c5008b673d3b39b019
Imran-Yoriichi <khajaimran.s10@gmail.com>
2026-04-05 19:05:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -79,7 +79,7 @@ resource "azurerm_log_analytics_workspace" "logs" {
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
   sku                 = "PerGB2018"
-  retention_in_days   = 7
+ 
 
   tags = {
     environment = "dev"
